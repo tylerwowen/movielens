f30a0342076d3bd44e979b7a11f2fbbeac8ff903
tyler@mbp <ouyang@cs.ucsb.edu>
2016-02-21 15:47:50 -0800

    Initial Commit

---
+++ moivelens/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  moivelens
+//
+//  Created by Tyler Weimin Ouyang on 2/21/16.
+//  Copyright © 2016 Tyler Ouyang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
