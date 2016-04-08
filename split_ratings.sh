da9fa4d3e0a918daec05486f0fca58d8e72d7296
tyler@mbp <ouyang@cs.ucsb.edu>
2016-04-07 22:17:36 -0700

    generate sorted datasets to improve memory locality

--- split_ratings.sh
+++ split_ratings.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+trap `rm -f tmp_base.$$ tmp_test.$$; exit 1` 1 2 15
 RATING_DATA="ratings_shuffled.dat"
 
 if [ ! -f $RATING_DATA ]; then
@@ -15,18 +16,22 @@ echo "remainder: $REMAINDER"
 
 for i in 1 2 3 4 5
   do
-    head -`expr $i \* $SET_SIZE` $RATING_DATA | tail -$SET_SIZE > u$i.test
+    head -`expr $i \* $SET_SIZE` $RATING_DATA | tail -$SET_SIZE > tmp_test.$$
 
     # XXX: OSX users will see the message "head: illegal line count -- 0" here,
     #      but this is just a warning; the script still works as intended.
-    head -`expr \( $i - 1 \) \* $SET_SIZE` $RATING_DATA > u$i.base
-    tail -`expr \( 5 - $i \) \* $SET_SIZE` $RATING_DATA >> u$i.base
+    head -`expr \( $i - 1 \) \* $SET_SIZE` $RATING_DATA > tmp_base.$$
+    tail -`expr \( 5 - $i \) \* $SET_SIZE` $RATING_DATA >> tmp_base.$$
+
 
     if [ $i -eq 5 ]; then
-       tail -$REMAINDER $RATING_DATA >> u5.test
+       tail -$REMAINDER $RATING_DATA >> tmp_test.$$
     else
-       tail -$REMAINDER $RATING_DATA >> u$i.base
+       tail -$REMAINDER $RATING_DATA >> tmp_base.$$
     fi
+    sed -e "s/::/ /g" tmp_test.$$ | sort -k 1,1n -k 2,2n > u$i.test
+    sed -e "s/::/ /g" tmp_base.$$ | sort -k 1,1n -k 2,2n > u$i.base
+    rm tmp_base.$$ tmp_test.$$
 
     echo "u$i.test created.  `wc -l u$i.test | xargs | cut -d " " -f 1` lines."
     echo "u$i.base created.  `wc -l u$i.base | xargs | cut -d " " -f 1` lines."
